fdbc947f102ed7389e8916e82e0e04e40524d427
subin <hs2907@naver.com>
2023-12-19 14:27:31 +0900

    test

---
+++ src/Kr/or/ddit/variable/VariableClass.java
@@ -0,0 +1,20 @@
+package Kr.or.ddit.variable;
+
+public class VariableClass {
+
+	
+		/*2.변수와 타입
+	  *  변수 선언 및 초기화
+	  *  변수 명명 규칙
+	  *  기본 타입 
+	  *  타입 변환
+	  *  변수와 시스텝 입출력
+		 * 
+		 * */
+	public static void main(String[] args) {
+		
+		/*
+		 * 변수 선언 및 초기화 */
+	}
+
+}
